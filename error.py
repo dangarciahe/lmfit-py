9b8b0949aa60a8c5b0ef188058f1587cf19ba4bb
Daniel Alejandro García Hernández <dangarciahe@unal.edu.co>
2025-10-16 17:24:26 +0000

    WIP(AMPGO): Add evaluations counter to the erro tester file

--- error.py
+++ error.py
@@ -1,6 +1,10 @@
 import lmfit
 
+counter = 0
+
 def parabolic(p):
+  global counter
+  counter += 1
   x = float(p["x"])
   y = x**2
   print (f"x: {x:.5f}, y:{y:.5f}")
@@ -12,6 +16,7 @@ fit_params.add('x', value=-2, min=-10, max=10, vary=True)
 result = lmfit.minimize(parabolic, fit_params, method="ampgo", max_nfev=100)
 #result = lmfit.minimize(parabolic, fit_params, method="ampgo", max_nfev=1000000)
 print(lmfit.fit_report(result))
+print(f"Pasadas ppor function: {counter}")
 
 # Mejor error alcanzado (raíz del chi-cuadrado)
 best_error = result.chisqr ** 0.5
