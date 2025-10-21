42b99c10775610bcb354b2ce271e5077dbbc722b
Daniel Alejandro García Hernández <dangarciahe@unal.edu.co>
2025-10-21 18:06:33 +0000

    feat(Test): Add seed parameter

--- error.py
+++ error.py
@@ -1,25 +1,24 @@
+import numpy as np
+
 import lmfit
+
+np.random.seed(42)
 
 counter = 0
 
+
 def parabolic(p):
-  global counter
-  counter += 1
-  x = float(p["x"])
-  y = x**2
-  print (f"x: {x:.5f}, y:{y:.5f}")
-  return y
+    global counter
+    counter += 1
+    x = float(p["x"])
+    y = x**2
+    print(f"x: {x:.5f}, y:{y:.5f}")
+    return y
+
 
 fit_params = lmfit.Parameters()
 fit_params.add('x', value=-2, min=-10, max=10, vary=True)
 
 result = lmfit.minimize(parabolic, fit_params, method="ampgo", max_nfev=100)
-#result = lmfit.minimize(parabolic, fit_params, method="ampgo", max_nfev=1000000)
+# result = lmfit.minimize(parabolic, fit_params, method="ampgo", max_nfev=1000000)
 print(lmfit.fit_report(result))
-print(f"Pasadas ppor function: {counter}")
-
-# Mejor error alcanzado (raíz del chi-cuadrado)
-best_error = result.chisqr ** 0.5
-print(f"Best error: {best_error}")
-
-#print(lmfit.__version__)
